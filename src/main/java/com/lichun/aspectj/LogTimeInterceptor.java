98a5feba9c550f73677bb222fdd9b751d8abe741
7doggy <seven.doggy@gmail.com>
2019-04-11 15:38:54 +0800

    remove unused packages

--- src/main/java/com/lichun/aspectj/LogTimeInterceptor.java
+++ src/main/java/com/lichun/aspectj/LogTimeInterceptor.java
@@ -5,8 +5,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Slf4j
